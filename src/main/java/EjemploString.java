2d366bd2bb008f5aac29d15409331ab5144ef72d
jpmvega98 <jpmvega1998@gmail.com>
2022-10-18 16:52:44 -0500

    prueba

--- src/main/java/EjemploString.java
+++ src/main/java/EjemploString.java
@@ -1,15 +1,15 @@
 public class EjemploString {
     public static void main(String[] args) {
-        String curso = "Programacion java";
-        String curso2 = new String("Programacion java");
-
+        String curso = "Programación Java";
+        String curso2 = new String("Programación Java");
+        
         boolean esIgual = curso == curso2;
         System.out.println("curso == curso2 = " + esIgual);
-
+        
         esIgual = curso.equals(curso2);
         System.out.println("curso.equals(curso2) = " + esIgual);
 
-        String curso3 = "programacion java";
+        String curso3 = "Programación Java";
         esIgual = curso == curso3;
         System.out.println("curso == curso3 = " + esIgual);
 
