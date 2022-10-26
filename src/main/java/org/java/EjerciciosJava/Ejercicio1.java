602cfd82c3f085a97a4ed5beb93e0e99b7fe5924
jpmvega98 <jpmvega1998@gmail.com>
2022-10-26 10:26:34 -0500

    hoy todo fue ejercicios desde el 1 hasta el 11 por el momento

--- src/main/java/org/java/EjerciciosJava/Ejercicio1.java
+++ src/main/java/org/java/EjerciciosJava/Ejercicio1.java
@@ -1,3 +1,4 @@
+//https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-basicos-java/
 //Declara dos variables numéricas (con el valor que desees), muestra por
 // consola la suma, resta, multiplicación, división y módulo (resto de la división).
 
