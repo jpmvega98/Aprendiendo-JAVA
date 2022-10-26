a0e6f822dcdd043346c9d63088dbdedec07af9f4
jpmvega98 <jpmvega1998@gmail.com>
2022-10-26 10:35:42 -0500

    todo por la manana

--- src/main/java/org/java/EjerciciosJava/Ejercicio12.java
+++ src/main/java/org/java/EjerciciosJava/Ejercicio12.java
@@ -4,7 +4,11 @@ package org.java.EjerciciosJava;
 public class Ejercicio12 {
     public static void main(String[] args) {
 
+        for (int i = 1; i < 101; i++) {
+            if (i % 2 == 0 || i % 3 == 0) {
+                System.out.println(i);
 
-
+            }
+        }
     }
 }
---
+++ src/main/java/org/java/EjerciciosJava/Ejercicio13.java
@@ -0,0 +1,14 @@
+/*
+13) Realiza una aplicación que nos pida un número de ventas a introducir,
+ después nos pedirá tantas ventas por teclado como número
+ de ventas se hayan indicado. Al final mostrara la suma de todas las ventas.
+ Piensa que es lo que se repite y lo que no.
+*/
+
+package org.java.EjerciciosJava;
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+
+    }
+}
